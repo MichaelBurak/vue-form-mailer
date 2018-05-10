e5c63f4a0796eb45a095613fd9199fa8e5993880
Michael Burak <michaelrburak@gmail.com>
2018-05-10 12:08:01 -0400

    router set up for auth integration

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,46 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+
 import Notifications from '@/components/Notifications'
+import Login from '@/components/Login'
+import firebase from 'firebase'
 
 Vue.use(Router)
 
-export default new Router({
+let router = new Router({
   routes: [
     {
+      path: '*',
+      redirect: '/login'
+    },
+    {
       path: '/',
+      redirect: '/login'
+    },
+    {
+      path: '/login',
+      name: 'Login',
+      component: Login
+    },
+
+    {
+      path: '/notificatoins',
       name: 'Notifications',
-      component: Notifications
+      component: Notifications,
+      meta: {
+        requiresAuth: true
+      }
     }
   ]
 })
+
+router.beforeEach((to, from, next) => {
+  let currentUser = firebase.auth().currentUser;
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth);
+
+  if (requiresAuth && !currentUser) next('login')
+  else if (!requiresAuth && currentUser) next('notifications')
+  else next()
+})
+
+export default router
