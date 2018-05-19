125f9eddfa4e578f6e40797e36bd0fcf9ffccefd
Michael Burak <michaelrburak@gmail.com>
2018-05-19 10:44:17 -0400

    refactoring main app and integrating auth

--- src/main.js
+++ src/main.js
@@ -3,23 +3,34 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
 import VueFire from 'vuefire'
-import firebase from 'firebase/app'
+import firebase from 'firebase'
 import 'firebase/firestore'
-Vue.use(VueFire)
-firebase.initializeApp({
-  databaseURL: "https://vue-form-mailer.firebaseio.com",
-  projectId: "vue-form-mailer"
-})
-export const db = firebase.firestore()
+
 
 Vue.config.productionTip = false
 
-/* eslint-disable no-new */
-new Vue({
-  el: '#app',
-  router,
-  components: { App },
-  template: '<App/>'
-})
+Vue.use(VueFire)
+let app;
+let config = {
+  apiKey: "AIzaSyA6EVtqx5rGG2unHGxigl6iYpTmKk6nHZY",
+  authDomain: "vue-form-mailer.firebaseapp.com",
+  databaseURL: "https://vue-form-mailer.firebaseio.com",
+  projectId: "vue-form-mailer",
+  storageBucket: "vue-form-mailer.appspot.com",
+  messagingSenderId: "1029262874157"
+};
+firebase.initializeApp(config);
+export const db = firebase.firestore()
+
+firebase.auth().onAuthStateChanged(function (user) {
+  if (!app) {
+    /* eslint-disable no-new */
+    app = new Vue({
+      el: '#app',
+      template: '<App/>',
+      components: { App },
+      router
+    })
+  }
+});
